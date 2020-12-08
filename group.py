6e32106ded87c1fb68f33c1b8690f107ab7422f7
JoyStickUi <mrfany2014@gmail.com>
2020-12-08 16:50:38 +0700

    half of second part

--- group.py
+++ group.py
@@ -1,6 +1,41 @@
 from flask import make_response, abort
 from config import db
-from models import Faculty, Teacher, Auditory, FacultySchema, TeacherSchema, AuditorySchema
+from models import User, Faculty, Teacher, FacultySchema, TeacherSchema, Auditory, AuditorySchema
 from user import create_token
 
-def read_all_fac():
+def read_all_fac(token):
+    user = User.query.filter(User.token == token).one_or_none()
+
+    if user is not None:
+        faculties = Faculty.query.order_by(Faculty.id).all()
+        faculty_schema = FacultySchema(many=True)
+        data = faculty_schema.dump(faculties).data
+        return data
+    else:
+        abort(403, "Not allowed")
+
+def read_one_fac(faculty_id, token):
+    pass
+
+def teachers(token):
+    user = User.query.filter(User.token == token).one_or_none()
+
+    if user is not None:
+        teachers = Teacher.query.order_by(Teacher.id).all()
+        teacher_schema = TeacherSchema(many=True)
+        data = teacher_schema.dump(teachers).data
+        return data
+    else:
+        abort(403, "Not allowed")
+
+def auditory(token):
+    user = User.query.filter(User.token == token).one_or_none()
+    if user is not None:
+        auditories = Auditory.query.order_by(Auditory.id).all()
+        auditory_schema = AuditorySchema(many=True)
+        data = auditory_schema.dump(auditories).data
+        return data
+    else:
+        abort(403, "Not allowed")
+
+
--- models.py
+++ models.py
@@ -14,6 +14,23 @@ class User(db.Model):
     expiration = db.Column(db.DateTime)
 
 
+class Faculty(db.Model):
+    __tablename__ = "faculty"
+    id = db.Column(db.Integer, db.Sequence("seq_fac_id"), primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+
+class Teacher(db.Model):
+    __tablename__ = "teacher"
+    id = db.Column(db.Integer, db.Sequence("seq_tech_id"), primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+class Auditory(db.Model):
+    __tablename__ = "auditory"
+    id = db.Column(db.Integer, db.Sequence("seq_aud_id"), primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+
+
 class UserSchema(ModelSchema):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -22,6 +39,7 @@ class UserSchema(ModelSchema):
         model = User
         sqla_session = db.session
         fields = ("id", "name", "login")
+
 
 class TokenSchema(ModelSchema):
     def __init__(self, **kwargs):
@@ -32,4 +50,29 @@ class TokenSchema(ModelSchema):
         sqla_session = db.session
         fields = ("id", "name", "login", "token")
 
+class FacultySchema(ModelSchema):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+    class Meta:
+        model = Faculty
+        sqla_session = db.session
+        fields = ("id", "name")
+
+class TeacherSchema(ModelSchema):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    class Meta:
+        model = Teacher
+        sqla_session = db.session
+        fields = ("id", "name")
+
+class AuditorySchema(ModelSchema):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    class Meta:
+        model = Auditory
+        sqla_session = db.session
+        fields = ("id", "name")
